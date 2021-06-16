190a7d7c667ba5bb66d6f6e322adfac25443c696
hsukid <xufaping@vanoda.net>
2021-06-16 18:08:05 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import {
   CubicEase,
   EasingFunction,
   Animation,
-  ArcRotateCamera
+  ArcRotateCamera,
+  UniversalCamera,
+  HemisphericLight
 } from 'babylonjs'
 import 'babylonjs-loaders'
 
@@ -18,26 +20,45 @@ const engine = new Engine(canvas as HTMLCanvasElement)
 
 const scene = new Scene(engine)
 
-SceneLoader.Append(
-  'assets/玄关0609测试.obj/',
-  '玄关0609.obj',
-  scene,
-  function () {
-    // Create a default arc rotate camera and light.
-    scene.createDefaultCameraOrLight(true)
-    const camera = <ArcRotateCamera>scene.activeCamera
-    camera.attachControl(canvas, false)
-    camera.checkCollisions = true
+// const uri = 'shiyan.gltf'
+const uri = '中鼎工程文件.gltf'
 
-    nav.navToXuanguan(scene)
+SceneLoader.Append('assets/', uri, scene, function (o) {
+  // Create a default arc rotate camera and light.
+  // scene.createDefaultCameraOrLight(false, false, true)
+  const light = new HemisphericLight('HemiLight', new Vector3(0, 1, 0), scene)
+  // const camera = <UniversalCamera>o.cameras[0]
+  const camera = new UniversalCamera('free', new Vector3(-10, 50, -10), scene)
+  scene.activeCamera = camera
+  // const camera = <UniversalCamera>scene.activeCamera
+  camera.attachControl(canvas, false)
+  // scene.gravity = new Vector3(0, -9.81, 0)
 
-    createUI(scene)
+  // camera.applyGravity = true
+  camera.ellipsoid = new Vector3(1, 1, 1)
 
-    engine.runRenderLoop(function () {
-      scene.render()
-    })
-  }
-)
+  // scene.collisionsEnabled = true
+  camera.checkCollisions = true
+
+  console.log(o)
+  // camera.checkCollisions = true
+
+  // camera.radius = 10
+  // camera.set
+  // camera.setTarget(Vector3.Zero())
+  // // camera.maxZ = 120
+  // camera.upperRadiusLimit = 20
+
+  // nav.navToXuanguan(scene)
+
+  // createUI(scene)
+
+  window.scenex = scene
+
+  engine.runRenderLoop(function () {
+    scene.render()
+  })
+})
 
 window.addEventListener('resize', function () {
   engine.resize()
